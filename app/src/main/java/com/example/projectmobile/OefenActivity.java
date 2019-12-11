d7cbbf752a523fdf81ecd1c6ce1c71fcaf4c5024
alex-bor <47328790+alex-bor@users.noreply.github.com>
2019-12-12 00:25:00 +0100

    New version all working buttons

---
+++ app/src/main/java/com/example/projectmobile/OefenActivity.java
@@ -0,0 +1,12 @@
+package com.example.projectmobile;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OefenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oefen);
+    }
+}
